8f5f13b5e3dcd43a1c61b3e95055b85b0debb9d1
victoriaprisco <p.victoria@wustl.edu>
2024-04-05 17:11:41 -0500

    project initialization

---
+++ backend/server.js
@@ -0,0 +1,9 @@
+const express = require('express')
+require("dotenv").config()
+const app = express()
+const port = process.env.PORT || 3000
+
+
+app.listen(port, () => {
+  console.log(`KWUR backend listening on port ${port}`)
+})
