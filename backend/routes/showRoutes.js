f1cb737da19436c91076ce9edc9ca0f0cde854f7
victoriaprisco <p.victoria@wustl.edu>
2024-05-07 23:45:06 -0500

    protected george time

--- backend/routes/showRoutes.js
+++ backend/routes/showRoutes.js
@@ -6,8 +6,13 @@ require("dotenv").config()
 
 const router = express.Router()
 
+const isGeorgeTime = (userFullName, choice) => {
+    const choiceStartHour = parseInt(choice.startTime.substring(0, 2))
+    const choiceEndHour = parseInt(choice.endTime.substring(0, 2))
+    return userFullName !== "George Yeh" && choice.day === "Saturday" && choiceStartHour >= 9 && choiceEndHour < 12
+}
 
-const checkTime = async (id, choices, showInfo) => {
+const checkTime = async (userFullName, id, choices, showInfo) => {
     if(choices.length === 0) {
         return false // we checked everything, none worked
     }
@@ -21,6 +26,10 @@ const checkTime = async (id, choices, showInfo) => {
             day: day,
             startTime: startTime,
             endTime: endTime
+        }
+        if(isGeorgeTime(userFullName, choice) === true) { // only check if time is available if it is not George's
+            choices.shift()
+            return checkTime(userFullName, id, choices, showInfo)
         }
         try {
             const result = await showModel.findOne({ showTime: showTime })
@@ -47,7 +56,7 @@ const checkTime = async (id, choices, showInfo) => {
                 }
             }
             choices.shift()
-            return checkTime(id, choices, showInfo)
+            return checkTime(userFullName, id, choices, showInfo)
         }
         catch (e) { 
             console.log(e)
@@ -69,7 +78,9 @@ router.post("/attemptCreateNewShow", (req, res) => {
         else {
             const choices = req.body.choices
             const showInfo = req.body.showInfo
-            checkTime(user._id, choices, showInfo).then(isTimeTaken => {
+            const userName = user.firstName + " " + user.lastName
+            console.log(userName)
+            checkTime(userName, user._id, choices, showInfo).then(isTimeTaken => {
                 console.log("Show was just attempted to be made. Was it successful?", isTimeTaken)
                 if(isTimeTaken === true) {
                     res.status(201).send({message: "used a choice"})
