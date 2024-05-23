8e33ccbd5f3885e860f43df53a788b165faefd7f
victoriaprisco <p.victoria@wustl.edu>
2024-05-23 13:36:32 -0400

    NO MORE MONOREPO

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+echo "DID YOU MAKE SURE ALL OF THE URLS ARE CORRECT?"
+echo "** BUILDING **"
+npm run build-frontend
+echo "** DEPLOYING **"
+aws s3 sync frontend/build s3://kwur.wustl.edu
+echo "** ALL DONE **"
