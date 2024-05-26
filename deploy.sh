e1e1c252434ab159f8992afc34f1a5c8d7f2502e
victoriaprisco <p.victoria@wustl.edu>
2024-05-26 15:00:20 -0400

    404

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,5 @@ echo "** BUILDING **"
 npm run build-frontend
 echo "** DEPLOYING **"
 aws s3 sync frontend/build s3://kwur.wustl.edu
+# aws s3 mv frontend/404.html s3://kwur.wustl.edu
 echo "** ALL DONE **"
